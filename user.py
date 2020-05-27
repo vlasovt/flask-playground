8eed08a3354640a4b8ad31187e0abf3d1399da63
Taras Vlasov <tarasvlasov@gmail.com>
2020-05-26 23:22:11 -0400

    added post, put and get items

--- user.py
+++ user.py
@@ -1,4 +1,5 @@
 import sqlite3
+from flask_restful import Resource, reqparse
 
 class User:
     def __init__(self, _id, username, password):
@@ -32,4 +33,33 @@ class User:
         connection.close()
         return user
 
+class UserRegister(Resource):
 
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+        type=str,
+        required=True,
+        help='This field cannot be blank!'
+    )
+    parser.add_argument('password',
+        type=str,
+        required=True,
+        help='This field cannot be blank!'
+    )
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+
+        if User.find_by_username(data['username']):
+            return {'message': 'This user already exists'}, 400
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        create_user = "INSERT INTO users VALUES (NULL, ?, ?)"
+        cursor.execute(create_user, (data['username'], data['password']))
+
+        connection.commit()
+        connection.close()
+
+        return {'Message': 'User created successfully'}
